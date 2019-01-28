e69b2ce40fcd5615f7bf21667795a2bb2c5e1336
james woolfenden <james.woolfenden@slalom.com>
2019-01-28 17:07:46 +0000

    also template latest script

---
+++ data.template_file.latest.tf
@@ -0,0 +1,13 @@
+data "template_file" "latest" {
+  template = "${file("${path.module}/template/setlatest.sh.template")}"
+
+  vars = {
+    name        = "${var.name}"
+    projectroot = "${var.projectroot}"
+  }
+}
+
+resource "local_file" "latest" {
+  content  = "${data.template_file.latest.rendered}"
+  filename = "setlatest.sh"
+}
