df29f3b0512a1c109ba435b1c9fa2e90019a9de8
James Woolfenden <James.Woolfenden@gmail.com>
2019-03-15 09:58:24 +0000

    plurals

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
-  bucket     = "${var.name}-${data.aws_caller_identity.current.account_id}-artifact"
+  bucket     = "${var.name}-${data.aws_caller_identity.current.account_id}-artifacts"
   bucketname = "${var.bucketname == "" ? local.bucket : var.bucketname}"
 }
