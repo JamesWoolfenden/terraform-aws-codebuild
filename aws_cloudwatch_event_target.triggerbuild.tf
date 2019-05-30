50f8e149621ebaa41ca2f9084c0e96dd5e3f1ad7
James Woolfenden <James.Woolfenden@gmail.com>
2019-05-30 11:47:38 +0100

    use name not arn

--- aws_cloudwatch_event_target.triggerbuild.tf
+++ aws_cloudwatch_event_target.triggerbuild.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudwatch_event_target" "triggerbuild" {
   count    = "${var.reponame == "" ? 0 : 1}"
-  rule     = "${aws_cloudwatch_event_rule.codechange.arn}"
+  rule     = "${aws_cloudwatch_event_rule.codechange.name}"
   arn      = "${aws_codebuild_project.project.arn}"
   role_arn = "${aws_iam_role.trigger.arn}"
 }
