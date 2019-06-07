3bcbc983ba8634d2102b936c7fb5863677e13702
James Woolfenden <James.Woolfenden@slalom.com>
2019-06-07 09:41:40 +0100

    outputs buggy

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,6 @@ output "artifact_bucket" {
   value = "${local.bucketname}"
 }
 
-output "codebuild_role" {
-  value = "${var.role == "" ? element(concat(aws_iam_role.codebuild.*.arn, list("")), 0) : element(concat(data.aws_iam_role.existing.*.arn, list("")), 0)}"
-}
-
 output "codebuild_role_name" {
   value = "${var.role == "" ? element(concat(aws_iam_role.codebuild.*.id, list("")), 0) : var.role}"
 }
