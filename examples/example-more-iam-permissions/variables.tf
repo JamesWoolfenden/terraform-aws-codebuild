f80a4fdad37f31a3bf756d33acaf3d78763d8608
James Woolfenden <James.Woolfenden@slalom.com>
2019-06-24 13:38:06 +0100

    retest examples

--- examples/example-more-iam-permissions/variables.tf
+++ examples/example-more-iam-permissions/variables.tf
@@ -15,27 +15,10 @@ variable "name" {
 
 variable "force_artifact_destroy" {}
 
-variable "environment" {
-  type = "list"
-}
-
-variable "build_timeout" {
-  description = "Timeout set for the build to run"
-}
-
-variable "sourcecode" {
-  type = "list"
-}
-
 variable "common_tags" {
   type = "map"
 }
 
-variable "env" {
-  description = "environment name for ssm"
-  type        = "string"
-}
-
-variable "type" {
+variable "source_location" {
   type = "string"
 }
--- examples/example-supplyrole/terraform.tfvars
+++ examples/example-supplyrole/terraform.tfvars
@@ -6,5 +6,5 @@ role                      = "WilburToolsAcctCodePipelineCloudFormationEURole"
 source_location        = "https://git-codecommit.eu-west-1.amazonaws.com/v1/repos/exampleA"
 
   common_tags={
-    name="exampleA"
+    name="exampleSupplyrole"
   }
