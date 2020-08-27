d5481e9d64465b65dad461022448d8816162f40a
James Woolfenden <James.Woolfenden@gmail.com>
2020-08-27 14:02:39 +0100

    fix for tf 0.13

--- example/examplea/provider.aws.tf
+++ example/examplea/provider.aws.tf
@@ -1,16 +1,3 @@
 provider "aws" {
-  region  = "eu-west-1"
-  version = "3.00.0"
-}
-
-provider "local" {
-  version = "1.2"
-}
-
-provider "template" {
-  version = "2.1"
-}
-
-provider "random" {
-  version = "~> 2.2"
+  region = "eu-west-1"
 }
---
+++ example/examplea/terraform.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      version = "3.2.0"
+      source  = "hashicorp/aws"
+    }
+    local = {
+      version = "1.2"
+    }
+
+    template = {
+      version = "2.1"
+    }
+
+    random = {
+      version = "~> 2.2"
+    }
+  }
+}
