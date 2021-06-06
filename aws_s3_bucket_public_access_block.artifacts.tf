c9f6679debd10159bcb69943ec4e8a2509e4a68e
James Woolfenden <James.Woolfenden@gmail.com>
2021-06-06 12:08:43 +0100

    refresh

--- aws_s3_bucket_public_access_block.artifacts.tf
+++ aws_s3_bucket_public_access_block.artifacts.tf
@@ -1,7 +1,7 @@
 
 resource "aws_s3_bucket_public_access_block" "artifacts" {
   count  = var.bucketname == "" ? 1 : 0
-  bucket = aws_s3_bucket_public_access_block.artifacts[0].id
+  bucket = aws_s3_bucket.artifacts[0].id
 
   block_public_acls       = true
   block_public_policy     = true
